2f0b08119be87f31db571de76477255528012e47
minbarix <32944218+minbarix@users.noreply.github.com>
2017-10-25 14:48:20 -0600

    Most class framework is made

--- orderClasses.java
+++ orderClasses.java
@@ -28,4 +28,44 @@ public class OrderLine{
     public int getQuantity(){
         return quantity;
     }
+    public double getPrice(){
+        return price;
+    }
+}
+
+public class Product{
+    private String name;
+    private double price;
+    private int productID;
+
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public int getProductID(){
+        return productID;
+    }
+
+}
+
+public class Customer{
+    private String name;
+    private String address;
+    private float creditRating;
+    private float discountRating;
+
+    public float getCreditRating(){
+        return creditRating;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public float getDiscountRating(){
+        return discountRating;
+    }
 }
